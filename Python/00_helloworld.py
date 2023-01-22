63431236f171af1c796c221e1a1b505b3f369c93
ZapataCamilo <zapatacamilo86@gmail.com>
2023-01-22 11:02:15 -0500

    Python

---
+++ Python/00_helloworld.py
@@ -0,0 +1,11 @@
+# Esto es un comentario:Hola Mundo
+# Mi primer Hola Mundo
+My_int = 5
+name, last_name, addres = 'Camilo', 'Zapata', 'Itagui'
+My_int_2 = 22
+My_strign = "Hola Milo"
+Nombre, Apellido, Pais = 'Camilo', 'Zapata', 'Colombia'
+print('Hola Python')
+print(My_strign)
+print('Hola soy:',name, last_name, 'y mi direccion es:',addres, 'y mi edad es:', My_int_2,'años')
+print('Hola soy', Nombre, Apellido,'y vivo en:', Pais)
